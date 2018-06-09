6881c94a8db55f78e7f704d66913bb41525ac617
nownabe <nownabe@gmail.com>
2018-06-09 12:54:38 +0900

    :sparkles: Make init.sh more idempotent

--- init.sh
+++ init.sh
@@ -4,9 +4,10 @@ set -eux
 
 mitamae_version=v1.5.6
 
+[[ -d ${HOME}/bin ]] || mkdir ${HOME}/bin
+
 : Install mitamae
-if [[ ! -x ${HOME}/bin ]]; then
-  mkdir ${HOME}/bin
+if [[ ! -x ${HOME}/bin/mitamae ]]; then
   curl -sSL -o ${HOME}/bin/mitamae https://github.com/itamae-kitchen/mitamae/releases/download/${mitamae_version}/mitamae-x86_64-linux
   chmod +x ${HOME}/bin/mitamae
 fi
@@ -16,4 +17,4 @@ sudo apt update -y
 type git || sudo apt install -y git
 
 : Clone recipes
-git clone https://github.com/nownabe/recipes.git ${HOME}/src/github.com/nownabe/recipes
+[[ -d ${HOME}/src/github.com/nownabe/recipes ]] || git clone https://github.com/nownabe/recipes.git ${HOME}/src/github.com/nownabe/recipes
