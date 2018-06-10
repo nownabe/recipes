92c609c7c681c121df9c7e9ba025a4dadd974453
nownabe <nownabe@gmail.com>
2018-06-10 23:57:37 +0900

    :sparkles: Avoid to download slack when already installed

--- cookbooks/slack/default.rb
+++ cookbooks/slack/default.rb
@@ -1,10 +1,11 @@
 slack_version = "3.2.1"
 
 package_path = File.join($tmpdir, "slack.deb")
+check_installation = "dpkg-query -f '${Status}' -W slack-desktop | grep -q 'install ok installed'"
 
 execute "Download slack" do
   command "curl -L -o #{package_path} https://downloads.slack-edge.com/linux_releases/slack-desktop-#{slack_version}-amd64.deb"
-  not_if "[ -e #{package_path} ]"
+  not_if "[ -e #{package_path} ] || #{check_installation}"
 end
 
 
@@ -17,5 +18,5 @@ end
 execute "Install slack" do
   user "root"
   command "dpkg -i #{package_path}"
-  not_if "dpkg-query -f '${Status}' -W slack-desktop | grep -q 'install ok installed'"
+  not_if check_installation
 end
