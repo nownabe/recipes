2b0d7fb039d2e774ee1a3b8b47b3f438d140f248
nownabe <nownabe@gmail.com>
2018-06-11 00:22:40 +0900

    :sparkles: Add fcitx; Flexible Input Method Framework
    To configure keyboard settings

--- cookbooks/packages/default.rb
+++ cookbooks/packages/default.rb
@@ -1,4 +1,6 @@
 [
+  "fcitx", # Flexible Input Method Framework
+  "fcitx-mozc", # Mozc engine for fcitx - Client of the Mozc input method
   "pinta", # Simple drawing/painting program
 ].each do |pkg|
   package pkg do
