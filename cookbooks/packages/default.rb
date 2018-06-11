560e44302dc8a1aedd312223cda867037b19aaf0
nownabe <nownabe@gmail.com>
2018-06-11 13:06:37 +0900

    :sparkles: Install ag

--- cookbooks/packages/default.rb
+++ cookbooks/packages/default.rb
@@ -3,6 +3,7 @@
   "fcitx", # Flexible Input Method Framework
   "fcitx-mozc", # Mozc engine for fcitx - Client of the Mozc input method
   "pinta", # Simple drawing/painting program
+  "silversearcher-ag", # very fast grep-like program, alternative to ack-grep
 ].each do |pkg|
   package pkg do
     user "root"
