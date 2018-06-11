7e09cd2982fb7d97664dc95ecc542b1d629b35f6
nownabe <nownabe@gmail.com>
2018-06-11 09:40:43 +0900

    :sparkles: Add commit template

--- cookbooks/git/default.rb
+++ cookbooks/git/default.rb
@@ -4,3 +4,7 @@ template "gitconfig" do
   path "/home/#{$secret.user}/.gitconfig"
   source "templates/gitconfig"
 end
+
+remote_file "/home/#{$secret.user}/.commit_template" do
+  source "files/commit_template"
+end
