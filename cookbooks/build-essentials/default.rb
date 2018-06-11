5a276ca442725ca6820ebcdeca61f429077d569d
nownabe <nownabe@gmail.com>
2018-06-11 11:46:59 +0900

    :sparkles: Add build essentials

--- cookbooks/build-essentials/default.rb
+++ cookbooks/build-essentials/default.rb
@@ -1,7 +1,14 @@
 %w(
   build-essential
+  libbz2-dev
+  libgdbm-dev
+  liblzma-dev
+  libncurses5-dev
   libreadline-dev
   libssl-dev
+  libsqlite3-dev
+  tcl-dev
+  tk-dev
   zlib1g-dev
 ).each do |pkg|
   package pkg do
