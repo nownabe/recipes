55a3227e9d34ef1a15c573dc4506a185f1e15788
nownabe <nownabe@gmail.com>
2018-06-15 17:24:39 +0900

    :sparkles: Install nfs-common

--- cookbooks/packages/default.rb
+++ cookbooks/packages/default.rb
@@ -2,6 +2,7 @@
   "byobu", # text window manager, shell multiplexer, integrated DevOps environment
   "fcitx", # Flexible Input Method Framework
   "fcitx-mozc", # Mozc engine for fcitx - Client of the Mozc input method
+  "nfs-common", #  NFS support files common to client and server
   "pinta", # Simple drawing/painting program
   "silversearcher-ag", # very fast grep-like program, alternative to ack-grep
 ].each do |pkg|
