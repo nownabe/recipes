068afe56dcb85e8b2c3e8217e8c6ffacc1e8acd5
nownabe <nownabe@gmail.com>
2018-06-18 15:59:02 +0900

    :sparkles: Install webp

--- cookbooks/packages/default.rb
+++ cookbooks/packages/default.rb
@@ -1,12 +1,8 @@
-[
-  "byobu", # text window manager, shell multiplexer, integrated DevOps environment
-  "fcitx", # Flexible Input Method Framework
-  "fcitx-mozc", # Mozc engine for fcitx - Client of the Mozc input method
-  "nfs-common", #  NFS support files common to client and server
-  "pinta", # Simple drawing/painting program
-  "silversearcher-ag", # very fast grep-like program, alternative to ack-grep
-].each do |pkg|
-  package pkg do
-    user "root"
-  end
-end
+["byobu", # text window manager, shell multiplexer, integrated DevOps environment\
+ "fcitx", # Flexible Input Method Framework\
+ "fcitx-mozc", # Mozc engine for fcitx - Client of the Mozc input method\
+ "nfs-common", #  NFS support files common to client and server\
+ "pinta", # Simple drawing/painting program\
+ "silversearcher-ag", # very fast grep-like program, alternative to ack-grep\
+ "webp" # Lossy compression of digital photographic images.\
+].each do |pkg| package pkg do user "root" end end
