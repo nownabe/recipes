b2e4cd5964c2268c744ca77e4c689fceff5bdde0
nownabe <nownabe@gmail.com>
2018-06-22 15:17:25 +0900

    :sparkles: Install graphviz

--- cookbooks/packages/default.rb
+++ cookbooks/packages/default.rb
@@ -1,6 +1,7 @@
 ["byobu", # text window manager, shell multiplexer, integrated DevOps environment\
  "fcitx", # Flexible Input Method Framework\
  "fcitx-mozc", # Mozc engine for fcitx - Client of the Mozc input method\
+ "graphviz", # rich set of graph drawing tools\
  "nfs-common", #  NFS support files common to client and server\
  "pinta", # Simple drawing/painting program\
  "silversearcher-ag", # very fast grep-like program, alternative to ack-grep\
