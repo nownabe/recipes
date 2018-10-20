540e2ab6ec433da6509480855995b4719c045244
nownabe <nownabe@gmail.com>
2018-10-21 01:36:33 +0900

    :bug: Fix asciinema cookbook
    Fix #2

--- cookbooks/asciinema/default.rb
+++ cookbooks/asciinema/default.rb
@@ -1,6 +1,6 @@
 execute "Add asciinema repository" do
   user "root"
-  command "apt-add-repository ppa:zanchey/asciinema"
+  command "apt-add-repository -y ppa:zanchey/asciinema"
   notifies :run, "execute[Update]", :immediately
   not_if "apt-cache policy | grep -q zanchey/asciinema"
 end
