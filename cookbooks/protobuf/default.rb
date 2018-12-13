a4e7a76e807f37d6ff773bc95cb6c90572d26d83
nownabe <nownabe@gmail.com>
2018-12-13 16:31:35 +0900

    :package: Install protoc-gen-grpc-web

--- cookbooks/protobuf/default.rb
+++ cookbooks/protobuf/default.rb
@@ -1,3 +1,9 @@
+grpc_web_version = "1.0.3"
+
+
+# --------
+
+
 dest = "/home/#{$secret.user}/src/github.com/google/protobuf"
 
 git dest do
@@ -13,4 +19,15 @@ execute "Install protoc" do
     make install
   CMD
   not_if "type protoc"
+end
+
+grpc_web_url = "https://github.com/grpc/grpc-web/releases/download/#{grpc_web_version}/protoc-gen-grpc-web-#{grpc_web_version}-linux-x86_64"
+grpc_web_path = "/home/#{$secret.user}/bin/protoc-gen-grpc-web"
+
+execute "Install grpc-web" do
+  command <<-CMD
+    curl -LSs -o #{grpc_web_path} #{grpc_web_url}
+    chmod +x #{grpc_web_path}
+  CMD
+  not_if "[ -x #{grpc_web_path} ]"
 end
