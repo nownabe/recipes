6ba25f2de112456124a8aa4094b7c8048950349e
nownabe <nownabe@gmail.com>
2019-07-08 15:40:59 +0900

    :package: Install blueman

--- cookbooks/packages/apt.rb
+++ cookbooks/packages/apt.rb
@@ -1,6 +1,7 @@
 [
   "apache2-utils",     # Apache HTTP Server (utility programs for web servers)
   "asunder",           # graphical audio CD ripper and encoder
+  "blueman",           # Graphical bluetooth manager
   "byobu",             # text window manager, shell multiplexer, integrated DevOps environment
   "cdrdao",            # records CDs in Disk-At-Once (DAO) mode
   "cue2toc",           # converts CUE files to cdrdao's TOC format
