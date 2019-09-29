f9fe0d09ed099b5a334ce7f80f2b844ecdec2a23
nownabe <nownabe@gmail.com>
2019-09-29 20:29:02 +0900

    :gear: Install Teensy udev rules

--- cookbooks/ergodox/default.rb
+++ cookbooks/ergodox/default.rb
@@ -1,3 +1,11 @@
 install_tar "teensy" do
   url "https://www.pjrc.com/teensy/teensy_linux64.tar.gz"
 end
+
+rules_path = "/etc/udev/rules.d/49-teensy.rules"
+rules_url = "https://www.pjrc.com/teensy/49-teensy.rules"
+
+execute "Download Teensy udev rules" do
+  user "root"
+  command "curl -LSs -o #{rules_path} #{rules_url}"
+end
