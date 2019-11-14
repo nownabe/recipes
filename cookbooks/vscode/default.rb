80686a812a0dcdfd222ed36f34602e3479ba048c
nownabe <nownabe@gmail.com>
2019-11-14 15:56:41 +0900

    :package: Install Auto Import

--- cookbooks/vscode/default.rb
+++ cookbooks/vscode/default.rb
@@ -39,6 +39,7 @@ end
   ms-vscode.Go
   oderwat.indent-rainbow
   philosowaffle.openapi-designer
+  steoates.autoimport
   vscodevim.vim
 ).each do |ext|
   execute "Install VS Code extension #{ext}" do
