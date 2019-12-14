8b5936d4c82882b9e133fe5ea20798476d3c20a8
nownabe <nownabe@gmail.com>
2019-12-15 00:15:37 +0900

    :bug: Fix docker cookbook

--- cookbooks/docker/default.rb
+++ cookbooks/docker/default.rb
@@ -1,3 +1,5 @@
+# https://docs.docker.com/install/linux/docker-ce/ubuntu/
+
 %w(apt-transport-https ca-certificates curl software-properties-common).each do |pkg|
   package pkg do
     user "root"
@@ -10,18 +12,26 @@ execute "Add Docker's official GPG key" do
   not_if "apt-key list | grep -q Docker"
 end
 
+# TODO: Change the repository. See the installation document.
+#       Now package for Ubuntu 19.10 is not released, so temporarily using repo for 19.04.
 execute "Add docker repository" do
   user "root"
   command <<-CMD
     add-apt-repository \
-      "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+      "deb [arch=amd64] https://download.docker.com/linux/ubuntu disco stable"
   CMD
   notifies :run, "execute[Update]", :immediately
   not_if "apt policy | grep -q docker"
 end
 
-package "docker-ce" do
-  user "root"
+%w(
+  docker-ce
+  docker-ce-cli
+  containerd.io
+).each do |pkg|
+  package pkg do
+    user "root"
+  end
 end
 
 service "docker" do
