1f8aa8f3001d1840156755400dd9ed1b8be69dd0
nownabe <nownabe@gmail.com>
2020-07-23 22:10:53 +0900

    Fix pip not_if

--- cookbooks/packages/pip.rb
+++ cookbooks/packages/pip.rb
@@ -5,6 +5,6 @@
 ).each do |pkg|
   execute "Install #{pkg}" do
     command $anyenv_cmd.call("pip install #{pkg}")
-    not_if "pip list | grep -q #{pkg}"
+    not_if $anyenv_cmd.call("pip list | grep -q #{pkg}")
   end
 end
