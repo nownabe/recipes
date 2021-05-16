85b4067da5eedf8f53817f3810af5c0a50adcc67
nownabe <nownabe@gmail.com>
2021-05-16 10:00:26 +0900

    Add .rubocop.yml

--- cookbooks/ruby/default.rb
+++ cookbooks/ruby/default.rb
@@ -4,3 +4,7 @@ template "#{home}/.gem/credentials" do
   mode "0600"
   source "templates/gem/credentials"
 end
+
+link "#{home}/.rubocop.yml" do
+  to File.expand_path("../files/rubocop.yml", __FILE__)
+end
