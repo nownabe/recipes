7c1fe7e70e9edb70a292c0f4f4d9e2a71998c04e
nownabe <nownabe@users.noreply.github.com>
2021-05-20 02:15:53 +0000

    Update gcloud

--- cookbooks/gcloud/default.rb
+++ cookbooks/gcloud/default.rb
@@ -1,8 +1,11 @@
-python_version = "2.7.15"
+python_version = "3.8.2"
+gcloud_version = "341.0.0"
+
 python_path = "#{$home}/.anyenv/envs/pyenv/versions/#{python_version}/bin/python"
+gcloud_url = "https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-#{gcloud_version}-linux-x86_64.tar.gz"
 gcloud_archive = File.join($tmpdir, "gcloud.tar.gz")
 
-execute "Install python 2.7" do
+execute "Install python 3.8.2" do
   command $anyenv_cmd.call("pyenv install #{python_version}")
   not_if $anyenv_cmd.call("pyenv versions | grep -q #{python_version}")
 end
@@ -10,7 +13,7 @@ end
 directory "#{$home}/opt"
 
 execute "Download gcloud" do
-  command "curl -sSL -o #{gcloud_archive} https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-204.0.0-linux-x86_64.tar.gz"
+  command "curl -sSL -o #{gcloud_archive} #{gcloud_url}"
   not_if "[ -d #{$home}/opt/google-cloud-sdk ] || [ -e #{gcloud_archive} ]"
 end
 
