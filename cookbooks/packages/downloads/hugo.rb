a73c0f7501036466721de1527db136983b863fa3
nownabe <nownabe@users.noreply.github.com>
2021-09-24 14:42:36 +0900

    Update Hugo

--- cookbooks/packages/downloads/hugo.rb
+++ cookbooks/packages/downloads/hugo.rb
@@ -1,7 +1,8 @@
-version = "0.62.0"
+version = "0.88.1"
 
-hugo_url = "https://github.com/gohugoio/hugo/releases/download/v#{version}/hugo_#{version}_Linux-64bit.tar.gz"
+hugo_url = "https://github.com/gohugoio/hugo/releases/download/v#{version}/hugo_extended_#{version}_Linux-64bit.tar.gz"
 
 install_tar "hugo" do
   url hugo_url
+  check args: version, expected: "v#{version}"
 end
