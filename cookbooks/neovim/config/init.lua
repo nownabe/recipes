4c6c3037bbe876761fafc43b1e3bfe6dd66e4634
nownabe <nownabe@users.noreply.github.com>
2022-07-30 15:44:14 +0900

    Add keymaps for telescope

--- cookbooks/neovim/config/init.lua
+++ cookbooks/neovim/config/init.lua
@@ -72,6 +72,8 @@ vim.keymap.set({'n'}, '<Leader>ff', '<cmd>Telescope find_files<cr>')
 vim.keymap.set({'n'}, '<Leader>fb', '<cmd>Telescope buffers<cr>')
 vim.keymap.set({'n'}, '<Leader>fg', '<cmd>Telescope live_grep<cr>')
 vim.keymap.set({'n'}, '<Leader>fh', '<cmd>Telescope help_tags<cr>')
+vim.keymap.set({'n'}, '<Leader>fc', '<cmd>Telescope commands<cr>')
+vim.keymap.set({'n'}, '<Leader>fk', '<cmd>Telescope keymaps<cr>')
 vim.keymap.set({'n'}, '<Leader>fn', '<cmd>Telescope file_browser<cr>')
 
 
