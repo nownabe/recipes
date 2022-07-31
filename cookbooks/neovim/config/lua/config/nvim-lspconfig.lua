f93b17686b9ff74acd345b7ccce6937e53574579
nownabe <nownabe@users.noreply.github.com>
2022-07-31 21:53:21 +0900

    Update lsp config

--- cookbooks/neovim/config/lua/config/nvim-lspconfig.lua
+++ cookbooks/neovim/config/lua/config/nvim-lspconfig.lua
@@ -1,8 +1,9 @@
 local on_attach = function(client, bufnr)
-  vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
-
   local bufopts = { noremap = true, silent = true, buffer = bufnr }
   vim.keymap.set('n', '<Leader>lh', vim.lsp.buf.hover, bufopts)
+  vim.keymap.set('n', '<Leader>lf', vim.lsp.buf.formatting, bufopts)
+  vim.keymap.set('n', '<Leader>rn', vim.lsp.buf.rename, bufopts)
+  vim.keymap.set('n', '<Leader>ca', vim.lsp.buf.code_action, bufopts)
 end
 
 require('mason').setup()
