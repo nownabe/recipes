9b24fdf37aab0d2b1b597975b73ea16d64dde4e6
nownabe <nownabe@users.noreply.github.com>
2022-08-03 21:05:02 +0900

    Refine lspconfig config

--- cookbooks/neovim/config/lua/config/nvim-lspconfig.lua
+++ cookbooks/neovim/config/lua/config/nvim-lspconfig.lua
@@ -9,13 +9,34 @@ end
 local capabilities = require('cmp_nvim_lsp')
   .update_capabilities(vim.lsp.protocol.make_client_capabilities())
 
-require('mason').setup()
-require('mason-lspconfig').setup()
+local default_opts = {
+  on_attach = on_attach,
+  capabilities = capabilities,
+}
+
+local lspconfig = require('lspconfig')
+
+require('mason').setup({
+  ui = {
+    icons = {
+      package_installed = "✓",
+      package_pending = "➜",
+      package_uninstalled = "✗",
+    },
+  },
+})
+
+require('mason-lspconfig').setup({
+  ensure_installed = {
+    "black",
+    "prettier",
+    "solargraph",
+  },
+  automatic_installation = true,
+})
+
 require('mason-lspconfig').setup_handlers({
   function (server_name)
-    require('lspconfig')[server_name].setup({
-      on_attach = on_attach,
-      capabilities = capabilities,
-    })
+    lspconfig[server_name].setup(default_opts)
   end,
 })
