9e1aeb7a9ee12236d8c0be3aeffd826f4e881185
nownabe <nownabe@users.noreply.github.com>
2022-08-06 14:42:31 +0900

    Configure flutter-tools

--- cookbooks/neovim/config/lua/config/flutter-tools.lua
+++ cookbooks/neovim/config/lua/config/flutter-tools.lua
@@ -1,1 +1,21 @@
-require('flutter-tools').setup()
+-- https://github.com/akinsho/flutter-tools.nvim#full-configuration
+
+require('flutter-tools').setup({
+  ui = {
+    border = 'rounded',
+  },
+  widget_guides = {
+    enabled = false,
+  },
+  closing_tags = {
+    prefix = '# ',
+    enabled = true,
+  },
+  -- lsp = {
+  --   color = {
+  --     enabled = true,
+  --     virtual_text = true,
+  --     virtual_text_str = '■',
+  --   },
+  -- },
+})
