65df157b48777c9488b414515f6e5aadd686b771
nownabe <nownabe@gmail.com>
2023-06-24 21:34:22 +0900

    Adapt to WSL

--- recipe.rb
+++ recipe.rb
@@ -10,6 +10,10 @@ include_recipe "./ext"
 include_recipe "./util"
 include_recipe "./variables"
 
+def is_wsl?
+  !!ENV["WSL_DISTRO_NAME"]
+end
+
 execute "Update" do
   user "root"
   command "apt update"
@@ -20,6 +24,8 @@ if ENV.key?("ONLY")
     include_recipe "./cookbooks/#{cookbook}"
   end
 else
+  # Default cookbooks
+
   # Essential recipes
   include_recipe "./cookbooks/apt"
 
@@ -29,10 +35,10 @@ else
 
   include_recipe "./cookbooks/asdf"
   # include_recipe "./cookbooks/asciinema"
-  include_recipe "./cookbooks/charles"
+  # include_recipe "./cookbooks/charles"
   include_recipe "./cookbooks/dart"
   include_recipe "./cookbooks/direnv"
-  include_recipe "./cookbooks/discord"
+  include_recipe "./cookbooks/discord" unless is_wsl?
   include_recipe "./cookbooks/docker"
   # include_recipe "./cookbooks/ergodox"
   include_recipe "./cookbooks/fzf"
@@ -40,20 +46,20 @@ else
   include_recipe "./cookbooks/github"
   include_recipe "./cookbooks/gitlab"
   include_recipe "./cookbooks/go"
-  include_recipe "./cookbooks/gsettings"
+  include_recipe "./cookbooks/gsettings" unless is_wsl?
   include_recipe "./cookbooks/haskell"
-  include_recipe "./cookbooks/helm"
-  include_recipe "./cookbooks/kubernetes"
+  # include_recipe "./cookbooks/helm"
+  # include_recipe "./cookbooks/kubernetes"
   include_recipe "./cookbooks/neovim"
-  include_recipe "./cookbooks/obs"
-  include_recipe "./cookbooks/protobuf"
+  include_recipe "./cookbooks/obs" unless is_wsl?
+  # include_recipe "./cookbooks/protobuf"
   include_recipe "./cookbooks/python"
   include_recipe "./cookbooks/ruby"
   include_recipe "./cookbooks/rust"
   # include_recipe "./cookbooks/shutter"
-  include_recipe "./cookbooks/slack"
-  include_recipe "./cookbooks/terraform"
-  include_recipe "./cookbooks/vscode"
+  include_recipe "./cookbooks/slack" unless is_wsl?
+  # include_recipe "./cookbooks/terraform"
+  include_recipe "./cookbooks/vscode" unless is_wsl?
   # include_recipe "./cookbooks/wine"
 
   # include_recipe "./cookbooks/packages"
